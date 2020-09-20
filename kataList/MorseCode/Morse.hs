bedabb4069e33f08441356ee20269995256a8142
Pedro La Rosa <pedrolarosad@gmail.com>
2020-09-21 00:14:33 +0200

    adding encodeMorseCode Function

--- kataList/MorseCode/Morse.hs
+++ kataList/MorseCode/Morse.hs
@@ -1,22 +1,30 @@
 module MorseCode.Morse
 ( Morse
 , decodeMorseCode
+, encodeMorseCode
 ) where
 
 import MorseCode.Utils (splitString)
 import qualified Data.Map as M
 import Data.Char (toLower)
+import Data.List (intersperse)
 
 type Morse = String
 
 letterToMorse :: M.Map Char Morse
-letterToMorse = M.fromList [('a', ".-"), ('b', "-..."), ('c', "-.-."), ('d', "-.."), ('e', "."), ('f', "..-."), ('g', "--."), ('h', "...."), ('i', ".."), ('j', ".---"), ('k', "-.-"), ('l', ".-.."), ('m', "--"), ('n', "-."), ('o', "---"), ('p', ".--."), ('q', "--.-"), ('r', ".-."), ('s', "..."), ('t', "-"), ('u', "..-"), ('v', "...-"), ('w', ".--"), ('x', "-..-"), ('y', "-.--"), ('z', "--.."), ('1', ".----"), ('2', "..---"), ('3', "...--"), ('4', "....-"), ('5', "....."), ('6', "-...."), ('7', "--..."), ('8', "---.."), ('9', "----."), ('0', "-----")]
+letterToMorse = M.fromList [('a', ".-"), ('b', "-..."), ('c', "-.-."), ('d', "-.."), ('e', "."), ('f', "..-."), ('g', "--."), ('h', "...."), ('i', ".."), ('j', ".---"), ('k', "-.-"), ('l', ".-.."), ('m', "--"), ('n', "-."), ('o', "---"), ('p', ".--."), ('q', "--.-"), ('r', ".-."), ('s', "..."), ('t', "-"), ('u', "..-"), ('v', "...-"), ('w', ".--"), ('x', "-..-"), ('y', "-.--"), ('z', "--.."), ('1', ".----"), ('2', "..---"), ('3', "...--"), ('4', "....-"), ('5', "....."), ('6', "-...."), ('7', "--..."), ('8', "---.."), ('9', "----."), ('0', "-----"), ('.', ".-.-.-"), (',', "--..--"), ('?', "..--.."),(' ', " ")]
 
 morseToLetter :: M.Map Morse Char
 morseToLetter = M.foldrWithKey (flip M.insert) M.empty letterToMorse
 
+charToMorse :: Char -> Maybe Morse
+charToMorse c = M.lookup (toLower c) letterToMorse
+
 morseToChar :: Morse -> Maybe Char
 morseToChar m = M.lookup m morseToLetter
+
+encodeMorseCode :: String -> Morse
+encodeMorseCode = concat . intersperse " " . concat . mapM charToMorse
 
 decodeMorseCode :: String -> String
 decodeMorseCode =  map maybeToString . map morseToChar . splitString ' '
--- kataList/MorseCode/MorseCodeSpec.hs
+++ kataList/MorseCode/MorseCodeSpec.hs
@@ -1,7 +1,7 @@
 module MorseCode.MorseCodeSpec (spec) where
 
 import Test.Hspec
-import MorseCode.Morse (decodeMorseCode, Morse)
+import MorseCode.Morse (decodeMorseCode, encodeMorseCode, Morse)
 import Text.Printf (printf)
 
 shouldBeTranslatedTo :: Morse -> String -> Spec
@@ -9,10 +9,28 @@ shouldBeTranslatedTo morse string =
     it (printf "should translate %s to %s" morse string) $
         decodeMorseCode morse `shouldBe` string
 
+shouldBeEncodedTo :: String -> Morse -> Spec
+shouldBeEncodedTo string morse =
+    it (printf "should encode latin alphabet %s to morse code%s" string morse) $
+        encodeMorseCode string `shouldBe` morse
+
 spec :: Spec
 spec = do
-  describe "Testing Morse Code" $ do
+  describe "Testing decodeMorseCode function" $ do
     ".-" `shouldBeTranslatedTo` "a"
     ".- -..." `shouldBeTranslatedTo` "ab"
+    ".-   -..." `shouldBeTranslatedTo` "a b"
+    "----- .---- ..--- ...-- ....- ..... -.... --... ---.. ----." `shouldBeTranslatedTo` "0123456789"
     ".... . -.--   .--- ..- -.. ." `shouldBeTranslatedTo` "hey jude"
     ".... . .-.. .-.. ---   -- .- .-. .. -. .-" `shouldBeTranslatedTo` "hello marina"
+    "..   -.. --- -. -   -.- -. --- .-- .-.-.-" `shouldBeTranslatedTo` "i dont know."
+
+  describe "Testing encodeMorseCode" $ do
+    "a" `shouldBeEncodedTo` ".-"
+    "A" `shouldBeEncodedTo` ".-"
+    "a b" `shouldBeEncodedTo` ".-   -..."
+    "Hello World" `shouldBeEncodedTo` ".... . .-.. .-.. ---   .-- --- .-. .-.. -.."
+    "hello world" `shouldBeEncodedTo` ".... . .-.. .-.. ---   .-- --- .-. .-.. -.."
+    "0123456789" `shouldBeEncodedTo` "----- .---- ..--- ...-- ....- ..... -.... --... ---.. ----."
+    "Hello, how are you?" `shouldBeEncodedTo` ".... . .-.. .-.. --- --..--   .... --- .--   .- .-. .   -.-- --- ..- ..--.."
+    "I dont know." `shouldBeEncodedTo` "..   -.. --- -. -   -.- -. --- .-- .-.-.-"

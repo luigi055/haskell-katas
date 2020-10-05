e0b2438a7b965daacfa92b7a855fa0be6e7c48f8
Pedro La Rosa <pedrolarosad@gmail.com>
2020-10-05 20:51:34 +0200

    removing duplicated constants

--- kataList/BowlingGame/BowlingGame.hs
+++ kataList/BowlingGame/BowlingGame.hs
@@ -2,17 +2,18 @@ module BowlingGame.BowlingGame (getScore) where
 
 import Data.Char (digitToInt, isDigit)
 
-replaceChar :: Char -> String -> String
-replaceChar c = filter (/=c)
-
 getScore :: String -> Int
-getScore = processScore . replaceChar '|'
+getScore = processScore . filter (/='|')
+
+strikeSymbol = 'X'
+spareSymbol = '/'
+emptySymbol = '-'
 
 parseBall :: Char -> Int
-parseBall b
-  | b == 'X' || b == '/' = 10
-  | b == '-' = 0
-  | otherwise = digitToInt b
+parseBall c
+  | c == strikeSymbol || c == spareSymbol = 10
+  | c == emptySymbol = 0
+  | otherwise = digitToInt c
 
 processScore :: String -> Int
 processScore [] = 0
@@ -25,8 +26,8 @@ processScore (c1:c2:cs)
   | isStrike c1 = parsedBallOne + parsedBallTwo + parsedBallThree + processScore (c2:cs)
   | isSpare c2 = parsedBallTwo + parsedBallThree + processScore cs
   | otherwise = parsedBallOne + processScore (c2:cs)
-    where isStrike c = c == 'X'
-          isSpare c = c == '/'
+    where isStrike c = c == strikeSymbol
+          isSpare c = c == spareSymbol
           isStrikeAndNextSpare = isStrike c1 && (isSpare . head) cs
           isStrikeWithBonus = isStrike c1 && length cs == 1
           isLastBallStrike = isStrike c2 && length cs == 1

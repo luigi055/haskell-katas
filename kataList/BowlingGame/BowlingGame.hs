9d43f1bbf34606b624d1e99beb42e1254f7dcc5a
Pedro La Rosa <pedrolarosad@gmail.com>
2020-10-05 18:52:23 +0200

    fixing Strike with bonus

--- kataList/BowlingGame/BowlingGame.hs
+++ kataList/BowlingGame/BowlingGame.hs
@@ -20,8 +20,10 @@ processScore (s1:[]) = parseBall s1
 processScore (s1:'X':[]) = 0
 processScore (s1:s2:[]) = parseBall s1 + processScore [s2]
 processScore (_:'/':s3:[]) = 10 + parseBall s3
+processScore ('X':s2:s3:[]) = 10 + parseBall s2 + parseBall s3
 processScore xs@(s1:s2:s3:ss)
   | s1 == 'X' && s3 == '/' = parseBall s1 + parseBall s3 + processScore (s3:ss)
   | s1 == 'X' = parseBall s1 + parseBall s2 + parseBall s3 + processScore (s2:s3:ss)
+  | s1 == '/' = parseBall s1 + parseBall s2 + processScore (s2:s3:ss)
   | s2 == '/' = parseBall s2 + parseBall s3 + processScore (s3:ss)
   | otherwise = parseBall s1 + processScore (s2:s3:ss)

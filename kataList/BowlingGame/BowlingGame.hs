8f58d44db6b773f3b63bfa162d900c03ae3b8217
Pedro La Rosa <pedrolarosad@gmail.com>
2020-10-05 23:51:07 +0200

    organizing tests and formatting the code

--- kataList/BowlingGame/BowlingGame.hs
+++ kataList/BowlingGame/BowlingGame.hs
@@ -6,8 +6,8 @@ getScore :: String -> Int
 getScore = processScore . filter (/='|')
 
 strikeSymbol = 'X'
-spareSymbol = '/'
-emptySymbol = '-'
+spareSymbol  = '/'
+emptySymbol  = '-'
 
 parseBall :: Char -> Int
 parseBall c
@@ -19,19 +19,20 @@ processScore :: String -> Int
 processScore [] = 0
 processScore [c1] = parseBall c1
 processScore (c1:c2:cs)
-  | length cs == 0 = parsedBallOne + processScore [c2]
-  | isSpareWithBonus = parsedBallTwo + parsedBallThree
-  | isStrikeWithBonus = parsedBallOne + parsedBallTwo + parsedBallThree
-  | isStrikeAndNextSpare = parsedBallOne + parsedBallThree + processScore (c2:cs)
-  | isStrike c1 = parsedBallOne + parsedBallTwo + parsedBallThree + processScore (c2:cs)
-  | isSpare c2 = parsedBallTwo + parsedBallThree + processScore cs
-  | otherwise = parsedBallOne + processScore (c2:cs)
-    where isStrike c = c == strikeSymbol
-          isSpare c = c == spareSymbol
-          isStrikeAndNextSpare = isStrike c1 && (isSpare . head) cs
-          isStrikeWithBonus = isStrike c1 && length cs == 1
-          isLastBallStrike = isStrike c2 && length cs == 1
-          isSpareWithBonus = isSpare c2 && length cs == 1
-          parsedBallOne = parseBall c1
-          parsedBallTwo = parseBall c2
-          parsedBallThree = parseBall . head $ cs
+  | areLastTwoBalls       = parsedBallOne + processScore [c2]
+  | isSpareWithBonus      = parsedBallTwo + parsedBallThree
+  | isStrikeWithBonus     = parsedBallOne + parsedBallTwo + parsedBallThree
+  | isStrikeAndNextSpare  = parsedBallOne + parsedBallThree + processScore (c2:cs)
+  | isStrike c1           = parsedBallOne + parsedBallTwo + parsedBallThree + processScore (c2:cs)
+  | isSpare c2            = parsedBallTwo + parsedBallThree + processScore cs
+  | otherwise             = parsedBallOne + processScore (c2:cs)
+    where isStrike c            = c == strikeSymbol
+          isSpare c             = c == spareSymbol
+          isStrikeAndNextSpare  = isStrike c1 && (isSpare . head) cs
+          isStrikeWithBonus     = isStrike c1 && length cs == 1
+          isLastBallStrike      = isStrike c2 && length cs == 1
+          isSpareWithBonus      = isSpare c2 && length cs == 1
+          areLastTwoBalls       = length cs == 0
+          parsedBallOne         = parseBall c1
+          parsedBallTwo         = parseBall c2
+          parsedBallThree       = parseBall . head $ cs
--- kataList/BowlingGame/BowlingGameSpec.hs
+++ kataList/BowlingGame/BowlingGameSpec.hs
@@ -11,21 +11,33 @@ scoresTo frames expected =
 
 spec :: Spec
 spec = do
-  describe "Testing getScore function" $ do
-      ("X|X|X|X|X|X|X|X|X|X||XX" `scoresTo` 300)
-      ("9-|9-|9-|9-|9-|9-|9-|9-|9-|9-||" `scoresTo` 90)
-      ("-9|-9|-9|-9|-9|-9|-9|-9|-9|9-||" `scoresTo` 90)
-      ("-9|9-|-9|-9|9-|-9|-9|9-|-9|-9||" `scoresTo` 90)
-      ("5/|5/|5/|5/|5/|5/|5/|5/|5/|5/||5" `scoresTo` 150)
-      ("54|63|45|36|72|27|18|81|18|54||" `scoresTo` 90)
-      ("X|7/|9-|X|-8|8/|-6|X|X|X||81" `scoresTo` 167)
-      ("91|91|93|93|92|92|91|21|91|91||" `scoresTo` 99)
-      ("91|9-|9-|9-|9-|9-|92|9-|9-|9-||" `scoresTo` 93)
-      ("9-|X|8-|9-|9-|9-|9-|9-|9-|9-||" `scoresTo` 98)
-      ("--|X|81|--|--|--|--|--|--|--||" `scoresTo` 28)
-      ("--|X|8-|1-|--|--|--|--|--|--||" `scoresTo` 27)
-      ("9-|8/|--|9-|9-|9-|9-|9-|9-|9-||" `scoresTo` 82)
-      ("9-|8/|--|9-|9-|9-|9-|9-|9-|9/||1" `scoresTo` 84)
-      ("--|8/|-1|--|--|--|--|--|--|--||" `scoresTo` 11)
-      ("--|8/|1-|--|--|--|--|--|--|--||" `scoresTo` 12)
-      ("9-|X|8-|9-|9-|9-|9-|9-|9-|X||23" `scoresTo` 104)
+  describe "strikes for all rolls" $ do
+    "X|X|X|X|X|X|X|X|X|X||XX" `scoresTo` 300
+
+  describe "frames with no special symbols" $ do
+    "54|63|45|36|72|27|18|81|18|54||" `scoresTo` 90
+    "91|91|93|93|92|92|91|21|91|91||" `scoresTo` 99
+
+  describe "all frames fails the second ball" $ do
+    "9-|9-|9-|9-|9-|9-|9-|9-|9-|9-||" `scoresTo` 90
+    "-9|-9|-9|-9|-9|-9|-9|-9|-9|9-||" `scoresTo` 90
+    "-9|9-|-9|-9|9-|-9|-9|9-|-9|-9||" `scoresTo` 90
+    "91|9-|9-|9-|9-|9-|92|9-|9-|9-||" `scoresTo` 93
+
+  describe "spares all frames" $ do
+    "5/|5/|5/|5/|5/|5/|5/|5/|5/|5/||5" `scoresTo` 150
+
+  describe "mix strikes with normal" $ do
+    "9-|X|8-|9-|9-|9-|9-|9-|9-|9-||"  `scoresTo` 98
+    "--|X|81|--|--|--|--|--|--|--||"  `scoresTo` 28
+    "--|X|8-|1-|--|--|--|--|--|--||"  `scoresTo` 27
+    "9-|X|8-|9-|9-|9-|9-|9-|9-|X||23" `scoresTo` 104
+
+  describe "mixture of spare and normal" $ do
+    "9-|8/|--|9-|9-|9-|9-|9-|9-|9-||"  `scoresTo` 82
+    "9-|8/|--|9-|9-|9-|9-|9-|9-|9/||1" `scoresTo` 84
+    "--|8/|-1|--|--|--|--|--|--|--||"  `scoresTo` 11
+    "--|8/|1-|--|--|--|--|--|--|--||"  `scoresTo` 12
+
+  describe "all combinations" $ do
+    "X|7/|9-|X|-8|8/|-6|X|X|X||81" `scoresTo` 167
